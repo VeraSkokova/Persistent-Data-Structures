3ec31cd430d30601be2c875eef0c2d213b5066b2
VeraSkokova <miss.smiley17@gmail.com>
2020-01-20 21:24:08 +0700

    Fix broken files

--- persistent-data-structures/src/main/java/ru/nsu/fit/modern_programming/persistent_data_structures/PersistentCollection.java
+++ persistent-data-structures/src/main/java/ru/nsu/fit/modern_programming/persistent_data_structures/PersistentCollection.java
@@ -1,14 +1,14 @@
 package ru.nsu.fit.modern_programming.persistent_data_structures;
 
-import java.io.IOException;
-
-public interface PersistentCollection<E> {
+public interface PersistentCollection<E> extends Iterable<E> {
 
     boolean isEmpty();
 
     int size();
 
     PersistentCollection<E> add(E e);
+
+    PersistentCollection<E> remove(Object e);
 
     boolean equals(Object o);
 
